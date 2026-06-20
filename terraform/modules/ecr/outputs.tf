d19fda493e6bef43cd91eaa21f5c74993628377d
marycynthia <chinecheremokeke807@gmail.com>
2026-06-20 11:24:24 +0100

    gssjjd

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,4 +1,4 @@
 output "repository_url" {
   description = "ECR repository URL"
-  value       = try(aws_ecr_repository.this[0].repository_url, null)
+  value       = aws_ecr_repository.this.repository_url
 }
